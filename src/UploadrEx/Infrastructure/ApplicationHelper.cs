162cc1f0e927b22c0ff5eb56c13cfb1884d3cb87
arkyzs <arkyzs@github.com>
2014-10-25 21:08:20 +0200

    cleaning repository

---
+++ src/UploadrEx/Infrastructure/ApplicationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadrEx.Infrastructure
+{
+  internal class ApplicationHelper
+  {
+    public static string GetAppDataFolder()
+    {
+      return Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    }
+  }
+}
